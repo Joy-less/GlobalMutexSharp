dba4e0c50fd749bca1e7bd3a40c1c6e0dd5f5624
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-01-15 01:44:07 +0000

    Initial commit

---
+++ GlobalMutexSharp.Tests/Tests.cs
@@ -0,0 +1,102 @@
+﻿namespace GlobalMutexSharp.Tests;
+
+public class Tests {
+    [Fact]
+    public void BasicTest() {
+        bool Success = false;
+
+        using GlobalMutex Mutex = new("GlobalMutex BasicTest");
+
+        using (Mutex.Acquire(TimeSpan.FromSeconds(5))) {
+            Success = true;
+        }
+
+        Assert.True(Success);
+    }
+    [Fact]
+    public void ReentrancyTest() {
+        bool Success = false;
+
+        using GlobalMutex Mutex = new("GlobalMutex ReentrancyTest");
+
+        using (Mutex.Acquire(TimeSpan.FromSeconds(5))) {
+            using (Mutex.Acquire(TimeSpan.FromSeconds(5))) {
+                using (Mutex.Acquire(TimeSpan.FromSeconds(5))) {
+                    Success = true;
+                }
+            }
+        }
+
+        Assert.True(Success);
+    }
+    [Fact]
+    public void MultiProcessSimulationTest() {
+        int Success = 0;
+
+        Thread ThreadA = new(() => {
+            try {
+                using GlobalMutex Mutex = new("GlobalMutex MultiProcessSimulationTest");
+
+                using (Mutex.Acquire(TimeSpan.FromSeconds(5))) {
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                    Success++;
+                }
+            }
+            catch { }
+        });
+        Thread ThreadB = new(() => {
+            try {
+                using GlobalMutex Mutex = new("GlobalMutex MultiProcessSimulationTest");
+
+                using (Mutex.Acquire(TimeSpan.FromSeconds(5))) {
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                    Success++;
+                }
+            }
+            catch { }
+        });
+
+        ThreadA.Start();
+        ThreadB.Start();
+
+        ThreadA.Join();
+        ThreadB.Join();
+
+        Assert.Equal(2, Success);
+    }
+    [Fact]
+    public void MultiProcessTimeoutSimulationTest() {
+        int Success = 0;
+
+        Thread ThreadA = new(() => {
+            try {
+                using GlobalMutex Mutex = new("GlobalMutex MultiProcessTimeoutSimulationTest");
+
+                using (Mutex.Acquire(TimeSpan.FromSeconds(1))) {
+                    Thread.Sleep(TimeSpan.FromSeconds(2));
+                    Success++;
+                }
+            }
+            catch { }
+        });
+        Thread ThreadB = new(() => {
+            try {
+                using GlobalMutex Mutex = new("GlobalMutex MultiProcessTimeoutSimulationTest");
+
+                using (Mutex.Acquire(TimeSpan.FromSeconds(1))) {
+                    Thread.Sleep(TimeSpan.FromSeconds(2));
+                    Success++;
+                }
+            }
+            catch { }
+        });
+
+        ThreadA.Start();
+        ThreadB.Start();
+
+        ThreadA.Join();
+        ThreadB.Join();
+
+        Assert.Equal(1, Success);
+    }
+}
---
+++ GlobalMutexSharp/GlobalMutex.cs
@@ -0,0 +1,112 @@
+﻿namespace GlobalMutexSharp;
+
+/// <summary>
+/// A re-entrant named mutex that can lock across processes.
+/// </summary>
+public sealed class GlobalMutex : IDisposable {
+    private readonly Mutex Mutex;
+    private readonly Lock Lock = new();
+    private int Depth = 0;
+
+    /// <summary>
+    /// The current entrancy depth.
+    /// </summary>
+    public int CurrentDepth => Depth;
+    /// <summary>
+    /// Whether the mutex is currently acquired.
+    /// </summary>
+    public bool IsAcquired => Depth > 0;
+
+    /// <summary>
+    /// Constructs a global mutex with the given name.
+    /// </summary>
+    /// <param name="Name">The mutex name, which will be automatically escaped and formatted.</param>
+    public GlobalMutex(string Name) {
+        Mutex = new Mutex(initiallyOwned: false, $"Global\\{Uri.EscapeDataString(Name)}");
+    }
+
+    /// <summary>
+    /// Releases the mutex and frees all resources used.
+    /// </summary>
+    public void Dispose() {
+        Depth = 0;
+        Mutex.Dispose();
+    }
+    /// <summary>
+    /// Enters the mutex, blocking up to the given <paramref name="Timeout"/> until it is acquired.
+    /// </summary>
+    /// <returns>
+    /// A <see cref="DisposeExiter"/> that will exit the mutex when disposed.
+    /// </returns>
+    /// <exception cref="TimeoutException"/>
+    public DisposeExiter Acquire(TimeSpan Timeout) {
+        if (!TryEnter(Timeout)) {
+            throw new TimeoutException("Timeout acquiring global mutex.");
+        }
+        return new DisposeExiter(this);
+    }
+    /// <inheritdoc cref="Acquire(TimeSpan)"/>
+    public Task<DisposeExiter> AcquireAsync(TimeSpan Timeout) {
+        return Task.Run(() => Acquire(Timeout));
+    }
+    /// <summary>
+    /// Tries to enter the mutex, blocking up to the given <paramref name="Timeout"/> until it is acquired.
+    /// </summary>
+    /// <returns>
+    /// <see langword="true"/> if the mutex was acquired, <see langword="false"/> otherwise.
+    /// </returns>
+    /// <param name="Exiter">A <see cref="DisposeExiter"/> that will exit the mutex when disposed.</param>
+    public bool TryAcquire(TimeSpan Timeout, out DisposeExiter Exiter) {
+        if (TryEnter(Timeout)) {
+            Exiter = new DisposeExiter(this);
+            return true;
+        }
+        Exiter = default;
+        return false;
+    }
+
+    private bool TryEnter(TimeSpan Timeout) {
+        lock (Lock) {
+            if (Depth > 0) {
+                Depth++;
+                return true;
+            }
+
+            try {
+                if (Mutex.WaitOne(Timeout, exitContext: false)) {
+                    Depth++;
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            catch (AbandonedMutexException) {
+                // The mutex was abandoned in another process, but it will still get acquired
+                return true;
+            }
+        }
+    }
+    private bool TryExit() {
+        lock (Lock) {
+            if (Depth <= 0) {
+                return false;
+            }
+
+            Depth--;
+
+            if (Depth <= 0) {
+                Mutex.ReleaseMutex();
+            }
+            return true;
+        }
+    }
+
+    public readonly struct DisposeExiter(GlobalMutex Mutex) : IDisposable {
+        public GlobalMutex Mutex { get; } = Mutex;
+
+        public void Dispose() {
+            Mutex.TryExit();
+        }
+    }
+}
